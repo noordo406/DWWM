0e325e58ee035c312bbd1270312295f3c6c436eb
noordo406 <leclaire.quentin@free.fr>
2024-02-02 11:53:26 +0100

    02/02

--- JavaScript/ExerciceAPI/script.js
+++ JavaScript/ExerciceAPI/script.js
@@ -1,182 +1,22 @@
 const apiKey = '065d243bf67a2e1f0d4e36d7'
-const currency_list = {
-    AED: "United Arab Emirates Dirham",
-    AFN: "Afghan Afghani",
-    ALL: "Albanian Lek",
-    AMD: "Armenian Dram",
-    ANG: "Netherlands Antillean Guilder",
-    AOA: "Angolan Kwanza",
-    ARS: "Argentine Peso",
-    AUD: "Australian Dollar",
-    AWG: "Aruban Florin",
-    AZN: "Azerbaijani Manat",
-    BAM: "Bosnia-Herzegovina Mark",
-    BBD: "Barbadian Dollar",
-    BDT: "Bangladeshi Taka",
-    BGN: "Bulgarian Lev",
-    BHD: "Bahraini Dinar",
-    BIF: "Burundian Franc",
-    BMD: "Bermudan Dollar",
-    BND: "Brunei Dollar",
-    BOB: "Bolivian Boliviano",
-    BRL: "Brazilian Real",
-    BSD: "Bahamian Dollar",
-    BTN: "Bhutanese Ngultrum",
-    BWP: "Botswanan Pula",
-    BYN: "Belarusian Ruble",
-    BZD: "Belize Dollar",
-    CAD: "Canadian Dollar",
-    CDF: "Congolese Franc",
-    CHF: "Swiss Franc",
-    CLP: "Chilean Peso",
-    CNY: "Chinese Yuan",
-    COP: "Colombian Peso",
-    CRC: "Costa Rican Colón",
-    CUP: "Cuban Peso",
-    CVE: "Cape Verdean Escudo",
-    CZK: "Czech Republic Koruna",
-    DJF: "Djiboutian Franc",
-    DKK: "Danish Krone",
-    DOP: "Dominican Peso",
-    DZD: "Algerian Dinar",
-    EGP: "Egyptian Pound",
-    ERN: "Eritrean Nakfa",
-    ETB: "Ethiopian Birr",
-    EUR: "Euro",
-    FJD: "Fijian Dollar",
-    FKP: "Falkland Islands Pound",
-    FOK: "Faroese Króna",
-    GBP: "British Pound Sterling",
-    GEL: "Georgian Lari",
-    GGP: "Guernsey Pound",
-    GHS: "Ghanaian Cedi",
-    GIP: "Gibraltar Pound",
-    GMD: "Gambian Dalasi",
-    GNF: "Guinean Franc",
-    GTQ: "Guatemalan Quetzal",
-    GYD: "Guyanaese Dollar",
-    HKD: "Hong Kong Dollar",
-    HNL: "Honduran Lempira",
-    HRK: "Croatian Kuna",
-    HTG: "Haitian Gourde",
-    HUF: "Hungarian Forint",
-    IDR: "Indonesian Rupiah",
-    ILS: "Israeli New Sheqel",
-    IMP: "Manx Pound",
-    INR: "Indian Rupee",
-    IQD: "Iraqi Dinar",
-    IRR: "Iranian Rial",
-    ISK: "Icelandic Króna",
-    JEP: "Jersey Pound",
-    JMD: "Jamaican Dollar",
-    JOD: "Jordanian Dinar",
-    JPY: "Japanese Yen",
-    KES: "Kenyan Shilling",
-    KGS: "Kyrgystani Som",
-    KHR: "Cambodian Riel",
-    KID: "Kiribati Dollar",
-    KMF: "Comorian Franc",
-    KRW: "South Korean Won",
-    KWD: "Kuwaiti Dinar",
-    KYD: "Cayman Islands Dollar",
-    KZT: "Kazakhstani Tenge",
-    LAK: "Laotian Kip",
-    LBP: "Lebanese Pound",
-    LKR: "Sri Lankan Rupee",
-    LRD: "Liberian Dollar",
-    LSL: "Lesotho Loti",
-    LYD: "Libyan Dinar",
-    MAD: "Moroccan Dirham",
-    MDL: "Moldovan Leu",
-    MGA: "Malagasy Ariary",
-    MKD: "Macedonian Denar",
-    MMK: "Myanmar Kyat",
-    MNT: "Mongolian Tugrik",
-    MOP: "Macanese Pataca",
-    MRU: "Mauritanian Ouguiya",
-    MUR: "Mauritian Rupee",
-    MVR: "Maldivian Rufiyaa",
-    MWK: "Malawian Kwacha",
-    MXN: "Mexican Peso",
-    MYR: "Malaysian Ringgit",
-    MZN: "Mozambican Metical",
-    NAD: "Namibian Dollar",
-    NGN: "Nigerian Naira",
-    NIO: "Nicaraguan Córdoba",
-    NOK: "Norwegian Krone",
-    NPR: "Nepalese Rupee",
-    NZD: "New Zealand Dollar",
-    OMR: "Omani Rial",
-    PAB: "Panamanian Balboa",
-    PEN: "Peruvian Nuevo Sol",
-    PGK: "Papua New Guinean Kina",
-    PHP: "Philippine Peso",
-    PKR: "Pakistani Rupee",
-    PLN: "Polish Zloty",
-    PYG: "Paraguayan Guarani",
-    QAR: "Qatari Rial",
-    RON: "Romanian Leu",
-    RSD: "Serbian Dinar",
-    RUB: "Russian Ruble",
-    RWF: "Rwandan Franc",
-    SAR: "Saudi Riyal",
-    SBD: "Solomon Islands Dollar",
-    SCR: "Seychellois Rupee",
-    SDG: "Sudanese Pound",
-    SEK: "Swedish Krona",
-    SGD: "Singapore Dollar",
-    SHP: "St. Helena Pound",
-    SLL: "Sierra Leonean Leone",
-    SOS: "Somali Shilling",
-    SRD: "Surinamese Dollar",
-    SSP: "South Sudanese Pound",
-    STN: "São Tomé and Príncipe Dobra",
-    SYP: "Syrian Pound",
-    SZL: "Swazi Lilangeni",
-    THB: "Thai Baht",
-    TJS: "Tajikistani Somoni",
-    TMT: "Turkmenistani Manat",
-    TND: "Tunisian Dinar",
-    TOP: "Tongan Pa'anga",
-    TRY: "Turkish Lira",
-    TTD: "Trinidad & Tobago Dollar",
-    TVD: "Tuvaluan Dollar",
-    TWD: "New Taiwan Dollar",
-    TZS: "Tanzanian Shilling",
-    UAH: "Ukrainian Hryvnia",
-    UGX: "Ugandan Shilling",
-    USD: "US Dollar",
-    UYU: "Uruguayan Peso",
-    UZS: "Uzbekistan Som",
-    VES: "Venezuelan Bolívar Soberano",
-    VND: "Vietnamese Dong",
-    VUV: "Vanuatu Vatu",
-    WST: "Samoan Tala",
-    XAF: "CFA Franc BEAC",
-    XCD: "East Caribbean Dollar",
-    XDR: "Special Drawing Rights",
-    XOF: "CFA Franc BCEAO",
-    XPF: "CFP Franc",
-    YER: "Yemeni Rial",
-    ZAR: "South African Rand",
-    ZMW: "Zambian Kwacha",
-    ZWL: "Zimbabwean dollar"
-};
+const currencylist = []
 
 async function initCurrencies(){
-    const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/AED`
+    const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/codes`
     try {
         const response = await fetch(apiUrl)
         const data = await response.json()
-        for (currency in data.conversion_rates) {
-            if (currency != "SLL") {let option = document.createElement("option")
-            option.textContent = `${currency}`
-            option.value = `${currency}`
+        for (currency of data.supported_codes) {
+            if (currency[0] != "SLE") {let option = document.createElement("option")
+            option.textContent = `${currency[0]}`
+            option.value = `${currency[0]}`
+            currencylist[currency[0]] = currency[1]
+            currency[0]
             let option2 = option.cloneNode(true)
-            if (currency == 'USD') {
+            if (currency[0] == 'USD') {
                 option.selected = true
             }
-            if (currency == 'EUR') {
+            if (currency[0] == 'EUR') {
                 option2.selected = true
             }
             document.querySelector("#monnaie1").appendChild(option)
@@ -204,7 +44,7 @@ document.querySelector('button').addEventListener('click', async function(){
     const currency2 = document.querySelector("#monnaie2").value
     const amount = document.querySelector("#input").value
     const rate = await convert(currency1, currency2)
-    document.querySelector("#output").innerHTML = `${amount} ${currency_list[currency1]} équivaut à ${parseFloat((amount*rate).toPrecision(14))} ${currency_list[currency2]}<br>(1 ${currency1} = ${rate} ${currency2})`
+    document.querySelector("#output").innerHTML = `${amount} ${currencylist[currency1]} équivaut à ${amount*rate} ${currencylist[currency2]}<br>(1 ${currency1} = ${rate} ${currency2})`
     
 })
 initCurrencies()
